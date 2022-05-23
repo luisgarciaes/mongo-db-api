ccba1f8f53ed7a418f625aa541c7e611786822b8
ruruisu <zaddozgl@gmail.com>
2022-05-23 12:21:07 -0500

    fixed bloodtype html form submit

--- routes/person.js
+++ routes/person.js
@@ -1,25 +1,27 @@
-let express = require('express');
-const mongoose = require('mongoose')
+let express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
-let Person = require('../models/person')
+let Person = require("../models/person");
 
-router.get('/persons', function(req,res, next){
-    Person.find(function(err,persons) {
-        if (err) return next(err);
-        res.json(persons)
-    })
-}
-)
-router.get('person',function (req,res){
-    res.render('person');
-})
-router.post('/addPerson', function(req,res){
-    const myPerson = new Person({
-        name: req.body.name,
-        age: req.body.age,
-        bloodType: req.body.bloodType,
-        ssn: req.body.ssn})
-        myPerson.save()
-})
+router.get("/persons", function (req, res, next) {
+  Person.find(function (err, persons) {
+    if (err) return next(err);
+    res.json(persons);
+  });
+});
 
-module.exports=router
+router.get("/person", function (req, res) {
+  res.render("person");
+});
+
+router.post("/addPerson", function (req, res) {
+  const myPerson = new Person({
+    name: req.body.name,
+    age: req.body.age,
+    bloodType: req.body.bloodType,
+    ssn: req.body.ssn
+  });
+  myPerson.save();
+});
+
+module.exports = router;
